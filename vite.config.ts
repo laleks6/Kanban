968774d48f100cfe63a998f90501caafcb909d82
laleks6 <lomanovsaha98@gmail.com>
2024-05-16 16:33:53 +0300

    fix: change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/To-do",
   plugins: [react()],
   test: {
     globals: true,
