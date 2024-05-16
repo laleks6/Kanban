fef364239da8995bd8f38f4b4dc2e7a4bd36407a
laleks6 <lomanovsaha98@gmail.com>
2024-05-16 10:52:48 +0300

    refactor: refactoring settings paint column

--- src/components/TodoComplex/column/settingsModal/SettingsModal.tsx
+++ src/components/TodoComplex/column/settingsModal/SettingsModal.tsx
@@ -7,19 +7,7 @@ import {
 import style from "./styles.module.scss";
 import binIcon from "../../../../assets/recycle-bin.png";
 import PaintTag from "../../paint/PaintTag";
-// import { Target, color } from "framer-motion";
-// const colors = {
-//   Gray: "#6a6a6a",
-//   Brown: "#603b2c",
-//   Orange: "#854c1d",
-//   Yellow: "#89632a",
-//   Green: "#2b593f",
-//   Blue: "#28456c",
-//   Purple: "#492f64",
-//   Pink: "#69314c",
-//   Red: "#6e3630",
-//   None: "",
-// };
+
 type Props = {
   index: number;
   status: boolean;
@@ -31,16 +19,9 @@ function SettingsModal({ index, status }: Props) {
   const changeColorTitile = (obj: Record<string, number | string>) =>
     dispatch(changeBackgroundColorTitle(obj));
   const remove = (indexColumn: number) => dispatch(removeColumn(indexColumn));
-  const onChangeRadioBtn = () =>
-    // e: React.FormEvent<HTMLInputElement>,
-    // color: string
-    {
-      // const target = e.target as HTMLInputElement;
-      // if (target.checked) {
-      //   changeColorTitile({ index, bgColor, textColor });
-      // }
-      changeColorTitile({ index, bgColor, textColor });
-    };
+  const onChangeRadioBtn = () => {
+    changeColorTitile({ index, bgColor, textColor });
+  };
   onChangeRadioBtn();
   const onClickRemove = () => {
     remove(index);
@@ -61,36 +42,8 @@ function SettingsModal({ index, status }: Props) {
         <PaintTag
           setBgColor={setBgColor}
           setTextColor={setTextColor}
-          styleModification=""
+          styleModification={style.paintMofification}
         />
-        {/* {Object.keys(colors).map((el) => {
-          if (typeof el === "string")
-            return (
-              <label htmlFor="s1-2" key={colors[el]} className={style.colorsSetting}>
-                <div className={style.colorBlock}>
-                  {colors[el] ? (
-                    <span
-                      className={style.colorIcon}
-                      style={{ backgroundColor: colors[el] }}
-                    />
-                  ) : (
-                    <img
-                      className={style.noneColorsIcon}
-                      src={noneIcon}
-                      alt="none"
-                    />
-                  )}
-                  <span>{el}</span>
-                </div>
-                <input
-                  type="radio"
-                  name={`radio_${index}`}
-                  onChange={(e) => onChangeRadioBtn(e, colors[el])}
-                />
-                <span className={style.newChecbox} />
-              </label>
-            );
-        })} */}
       </div>
     </div>
   );
