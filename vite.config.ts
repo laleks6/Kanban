3b6323fc65d6fa1e48f2684f7b963cb0169e78ef
laleks6 <lomanovsaha98@gmail.com>
2024-05-16 17:47:19 +0300

    docs: change gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/To-do",
+  base: "/To-do/",
   plugins: [react()],
   test: {
     globals: true,
